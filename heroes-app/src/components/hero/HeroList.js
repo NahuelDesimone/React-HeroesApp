5a1b4b6d3648cdceb6ecb385c1813e9079ebab40
NahuelDesimone <nahuel.desimone98@gmail.com>
2022-03-28 22:21:53 -0300

    Se crea el componente HeroScreen

--- heroes-app/src/components/hero/HeroList.js
+++ heroes-app/src/components/hero/HeroList.js
@@ -1,14 +1,18 @@
 import React from "react";
 import { getHeroesByPublisher } from "../../selectors/getHeroesByPublisher";
+import { HeroCard } from "./HeroCard";
 
 export const HeroList = ({ publisher }) => {
   const heroes = getHeroesByPublisher(publisher);
 
   return (
-    <ul>
+    <div className="row rows-cols-1 row-cols-md-3 g-3">
       {heroes.map((hero) => (
-        <li key={hero.id}>{hero.superhero}</li>
+        <HeroCard 
+          key={hero.id}
+          {...hero} //Asi le paso todas las propiedades que necesita recibir el componente HeroCard
+          />
       ))}
-    </ul>
+    </div>
   );
 };
--- heroes-app/src/components/hero/HeroScreen.js
+++ heroes-app/src/components/hero/HeroScreen.js
@@ -1,9 +1,24 @@
 import React from 'react'
+import { Navigate, useParams } from 'react-router-dom'
+import { getHeroById } from '../../selectors/getHeroById';
 
 export const HeroScreen = () => {
+
+  const {heroeID} = useParams();
+
+  const hero = getHeroById(heroeID);
+
+  if (!hero) {
+    return <Navigate to='/'/>
+  }
+
+  
   return (
     <div>
         <h1>Hero Screen</h1>
+        <p>
+          {hero.superhero}
+        </p>
     </div>
   )
 }
