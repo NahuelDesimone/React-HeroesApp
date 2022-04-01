114950a92d13e7c5be9d4c2bfb1ab099d52d2d43
NahuelDesimone <nahuel.desimone98@gmail.com>
2022-04-01 19:16:58 -0300

    Se agregan los estilos al componente HeroScreen

--- heroes-app/src/components/hero/HeroScreen.js
+++ heroes-app/src/components/hero/HeroScreen.js
@@ -1,24 +1,54 @@
-import React from 'react'
-import { Navigate, useParams } from 'react-router-dom'
-import { getHeroById } from '../../selectors/getHeroById';
+import React from "react";
+import { Navigate, useNavigate, useParams } from "react-router-dom";
+import { getHeroById } from "../../selectors/getHeroById";
 
 export const HeroScreen = () => {
-
-  const {heroeID} = useParams();
+  const { heroeID } = useParams();
 
   const hero = getHeroById(heroeID);
 
+  const navigate = useNavigate();
+
+  const handleReturn = () => {
+    navigate(-1); //Con el -1 le indico que vuelva a la pagina que estaba antes
+  };
+
   if (!hero) {
-    return <Navigate to='/'/>
+    return <Navigate to="/" />;
   }
 
-  
+  const { id, superhero, publisher, alter_ego, first_appearance, characters } =
+    hero;
+
+  const imagePath = `/assets/${id}.jpg`;
+
   return (
-    <div>
-        <h1>Hero Screen</h1>
-        <p>
-          {hero.superhero}
-        </p>
+    <div className="row mt-5">
+      <div className="col-4">
+        <img src={imagePath} alt={superhero} className="img-thumbnail" />
+      </div>
+
+      <div className="col-8">
+        <h3>{superhero}</h3>
+        <ul className="list-group list-group-flush">
+          <li className="list-group-item">
+            <b>Alter ego: </b> {alter_ego}{" "}
+          </li>
+          <li className="list-group-item">
+            <b>Publisher: </b> {publisher}{" "}
+          </li>
+          <li className="list-group-item">
+            <b>First Appearance: </b> {first_appearance}{" "}
+          </li>
+        </ul>
+
+        <h5 className="mt-3">Characters</h5>
+        <p>{characters}</p>
+
+        <button className="btn btn-outline-info" onClick={handleReturn}>
+          Regresar
+        </button>
+      </div>
     </div>
-  )
-}
+  );
+};
